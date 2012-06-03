14d71e8e5fdf8bb07e38b47a3e5c5657d9f6c0df
anton kallenberg <anton.kallenberg@gmail.com>
2012-06-03 19:47:58 +0200

    adding copy task to psake script

--- default.ps1
+++ default.ps1
@@ -1,27 +1,40 @@
 properties {
-  $testMessage = 'Executed Test!'
-  $compileMessage = 'Executed Compile!'
+  $configuration = "Release"
 }
 
 task default -depends CopyFiles
 
 task CopyFiles -depends Test {
+	$source = '.\MvcCiTest'
+	$destination = '.\Build'
+	$excludeFiles = @('*.pdb','*.cs','*.csproj','*.csproj.user','*.sln','.gitignore')
 	
+	$items = get-childitem $source -recurse -exclude $excludeFiles
+    foreach ($item in $items)
+    {
+        $target = join-path $destination $item.FullName.Substring($source.Length)
+        $doesTargetExist = -not($item.PSIsContainer -and (test-path($target)));
+		if ($doesTargetExist)
+        {
+            copy-item -path $item.FullName -destination $target
+        }
+    }
 }
 
 task Test -depends Compile, Setup { 
-  $testMessage
+  
 }
 
 task Compile -depends Setup { 
-  msbuild .\MvcCiTest.sln /p:Configuration=Release
+  msbuild /t:Clean /t:Build /p:Configuration=$configuration /v:q /nologo
 }
 
 task Setup { 
 	if ((Test-Path -path .\Build)) {
-		Remove-Item .\Build -Recurse	
+		dir '.\Build' -recurse | where {!@(dir -force $_.fullname)} | rm -whatif
+		Remove-Item '.\Build' -Recurse	
 	}
-	New-Item -Path '.\Build ' -ItemType "directory"
+	New-Item -Path '.\Build' -ItemType "directory"
 }
 
 task ? -Description "Helper to display task info" {
