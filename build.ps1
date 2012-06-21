4553058b0663f07638f89223f8fa04aff8a5d40d
anton kallenberg <anton.kallenberg@gmail.com>
2012-06-21 13:01:34 +0200

    replacing bat with powershell script

---
+++ build.ps1
@@ -0,0 +1,17 @@
+param(
+	[parameter(Mandatory=$false)]
+    [alias("e")]
+    $Environment
+)
+
+function build() {
+	if([string]::IsNullOrEmpty($Environment) -or $Environment -ieq 'debug') {
+		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Debug'}" "Default" "4.0"
+	}
+	if($Environment -ieq 'staging') {
+		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Release'; environment='Staging'}" "Staging" "4.0"
+	}
+	Write-Host "$Environment build done!"
+}
+
+build
