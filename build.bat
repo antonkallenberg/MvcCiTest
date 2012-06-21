8ec769e4f35486886135afecd5beb32f4cf65b44
anton kallenberg <anton.kallenberg@gmail.com>
2012-06-21 11:39:36 +0200

    adding configuration and environment handling

--- build.bat
+++ build.bat
@@ -1,1 +1,15 @@
-powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1" "default" "4.0"
+@echo off
+if "%1" == "" goto debug
+if "%1" == "debug" goto debug
+if "%1" == "staging" goto staging
+
+:debug
+powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Debug'}" "Default" "4.0"
+goto done
+
+:staging
+powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Release'; environment='Staging'}" "Staging" "4.0"
+goto done
+
+:done
+echo "Build done!"
--- src/buildScripts/build.ps1
+++ src/buildScripts/build.ps1
@@ -1,15 +1,26 @@
 properties {
 	$configuration = 'Release'
+	$environment = 'Debug'
 	$source = '..\MvcCiTest'
 	$destination = '..\..\Build'
 	$sln = '..\MvcCiTest.sln'
 	$specsBaseDir = '..\MvcCiTest.Tests.Mspec' 
 }
 
-task default -depends CopyFiles
+task Default -depends CopyFiles
+
+task Staging -depends CopyFilesToStagingFtp 
+
+task CopyFilesToStagingFtp  -depends MergeConfiguration {
+	Write-Host "copy to ftp"
+}
+
+task MergeConfiguration -depends CopyFiles { 
+	robocopy "$source\Configurations\$environment\" $destination /E
+}
 
 task CopyFiles -depends Test {
-	robocopy $source $destination /MIR /XD obj /XF *.pdb *.cs *.csproj *.csproj.user *.sln .gitignore
+	robocopy $source $destination /MIR /XD obj bundler Configurations Properties /XF *.pdb *.cs *.csproj *.csproj.user *.sln .gitignore README.txt packages.config
 }
 
 task Test -depends Compile, Setup { 
