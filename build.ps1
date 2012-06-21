dbc46957ff33ce0380a8be8a472023138039e89e
anton kallenberg <anton.kallenberg@gmail.com>
2012-06-21 13:13:51 +0200

    rename

--- build.ps1
+++ build.ps1
@@ -6,10 +6,10 @@ param(
 
 function build() {
 	if([string]::IsNullOrEmpty($Environment) -or $Environment -ieq 'debug') {
-		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Debug'}" "Default" "4.0"
+		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\builder.ps1 -properties @{configuration='Debug'}" "Default" "4.0"
 	}
 	if($Environment -ieq 'staging') {
-		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\build.ps1 -properties @{configuration='Release'; environment='Staging'}" "Staging" "4.0"
+		powershell .\src\buildScripts\psake.ps1 ".\src\buildScripts\builder.ps1 -properties @{configuration='Release'; environment='Staging'}" "Staging" "4.0"
 	}
 	Write-Host "$Environment build done!"
 }
